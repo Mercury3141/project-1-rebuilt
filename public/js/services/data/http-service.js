16913e1d885f01b0f35bc9e2dc1121a72cd98fc0
Mercury3141 <66523675+Mercury3141@users.noreply.github.com>
2024-07-14 15:53:09 +0200

    Defined http-service.

---
+++ public/js/services/data/http-service.js
@@ -0,0 +1,13 @@
+class HttpService {
+    async ajax(method, url, data, headers) {
+        const fetchHeaders = new Headers({'content-type': 'application/json', ...(headers || {})});
+
+        const x = await fetch(url, {
+            method: method,
+            headers: fetchHeaders, body: JSON.stringify(data)
+        });
+        return await x.json();
+    }
+}
+
+export const httpService = new HttpService();
