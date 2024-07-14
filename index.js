6330553a60eee84fe0e3f6a0fa99383fea6dcf40
Mercury3141 <66523675+Mercury3141@users.noreply.github.com>
2024-07-14 14:20:49 +0200

    server running.

---
+++ index.js
@@ -0,0 +1,11 @@
+import dotenv from "dotenv";
+
+dotenv.config({ path: `.env${process.env.NODE_ENV ? `-${process.env.NODE_ENV}` : ''}`});
+
+const app = (await import('./app.js')).app;
+
+const hostname = '127.0.0.1';
+const port = process.env.PORT || 3000;
+app.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
